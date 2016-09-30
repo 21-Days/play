f3c4ad0f8277e807b1f66676a8aa250cfbef3d1d
roadkillsanta <ericnie7@gmail.com>
2016-09-30 16:22:30 -0400

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ global file
 def interpreter(fille, split):
 	if(split!=""):
 		if(split.upper()=="WAY0" or split.upper()=="WAY 0")
-		
+			file=open(fille+"0.txt",r)
 		elif(split.upper()=="WAY1" or split.upper()=="WAY 1")
-			
+			file=open(fille+"1.txt",r)
 		else:
 			input("ERROR")
 			exit()
@@ -18,7 +18,7 @@ def interpreter(fille, split):
 	int=0
 	while(True)
 		line=story[int]
-		if("INPUT" in line.upper())
+		if("INPUT" in line)
 			foo=line.upper().replace("INPUT ", "")
 			lsts=foo.split(',', 1)
 			while(var==""):
@@ -47,13 +47,16 @@ def interpreter(fille, split):
 			lolls=jklm.replace(jkl+"=", "")
 			loolls=line.replace("+ "+jkl+" +", lolls)
 			input(loolls)
-		elif("SPLIT " in line.upper()):
+		elif("SPLIT " in line):
 			jklomp=line.replace("SPLIT ", "")
 			jollo=jklomp.split(',' ,1)
 			jol=jollo.get[0]
 			llo=jollo.get[1]
 			interpreter(jol, llo)
-		elif():
+		elif("SKIP" in line):
+			lomlom=line.replace("SKIP ", "")
+			ple=Int(lomlom)
+			file.seek(ple, 0)
 		else:
 			input(line)
 		int=int+1
