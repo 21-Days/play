059762056613e5680004162b2b2f043835fd6bf4
roadkillsanta <ericnie7@gmail.com>
2016-10-01 18:58:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ def interpreter(fille, split):
 			input("ERROR")
 			exit()
 	else:
+		print("21 DAYS")
+		time.sleep(4)
+		print("A GAME MADE BY EN, WJ, JZ")
 		file=open(fille, 'r')
 	temp=open("temp.txt", 'wb')
 	story=file.readlines()
@@ -34,6 +37,8 @@ def interpreter(fille, split):
 			scene=currS.get[1]
 			option=currS.get[2]
 			print("CHAPTER "+chapter+", SCENE "+scene+", OPTION "+option)
+			temp.seek(0)
+			temp.write("currentSection="+cu)
 			time.wait(3)
 		elif("+ " and " +" in line):
 			j=line.split(' + ', 1)
@@ -73,29 +78,5 @@ def interpreter(fille, split):
 	input("THANKS FOR PLAYING")
 	temp.close()
 	file.close()
-def start():
-	print("21 DAYS")
-	time.sleep(4)
-	print("A GAME MADE BY WILL JOHNSON AND JONATHAN ZHANG")
-	print("SPECIAL THANKS TO ERIC NIE")
-	intro.part1()
-	name = input("What's your name?")
-	while name == "":
-		name = input("What's your name?")
-	intro.part2(name)
-	ahead = response = input('Are you sure you want her to go ahead?')
-	while ahead.upper() != 'YES' and ahead.upper() != 'Y' and ahead.upper() != 'NO' and ahead.upper() != 'N':
-		ahead = response = input('Are you sure you want her to go ahead?')
-	if (ahead.upper() == 'YES' or ahead.upper() == 'Y'):
-		assert isinstance(name, object)
-		s1o1.way0(name)
-	if ahead.upper() == 'NO' or ahead.upper() == 'N':
-		direction = s1o1.way1(name)
-		while direction.contains().upper() != 'C' and direction.contains().upper() != 'S':
-			direction = response = input("Which way will you go?")
-		if direction.upper() == 'CITY' or direction.lower() == 'city':
-			s2o1.way1()
-		else:
-			s2o1.way2()
 class main():
-	start()
+	interpreter("storyline.txt")
